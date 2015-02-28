4a8ece952b5c331bb077bdf270c810a4207ee316
a1exb1 <alexbechmann@Alexs-MacBook-Pro.local>
2015-02-28 16:18:45 +0000

    pusher send/receive test working

---
+++ PusherTestSwift/PusherTestSwift-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  PusherTestSwift-Bridging-Header.h
+//  PusherTestSwift
+//
+//  Created by Alex Bechmann on 28/02/2015.
+//  Copyright (c) 2015 Alex Bechmann. All rights reserved.
+//
+
+#import <Pusher/Pusher.h>
+#import <Pusher/PTPusherEventDispatcher.h>
+#import <Pusher/PTPusherChannel.h> // ?
+
+
+#ifndef PusherTestSwift_PusherTestSwift_Bridging_Header_h
+#define PusherTestSwift_PusherTestSwift_Bridging_Header_h
+
+
+#endif
--- PusherTestSwift/ViewController.swift
+++ PusherTestSwift/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PTPusherDelegate {
 
+    var client:AnyObject? = nil
+    var channel: PTPusherChannel = PTPusherChannel()
+    var api: PTPusherAPI = PTPusherAPI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "send")
+        
+        self.client = PTPusher.pusherWithKey("3bed0224e216f7d3c3a4", delegate: self)
+        self.client?.connect()
+        self.api = PTPusherAPI(key: "3bed0224e216f7d3c3a4", appID: "109110", secretKey: "c1e850ac3781ed00bf63")
+        
+        self.channel = self.client!.subscribeToChannelNamed("50")
+        self.channel.bindToEventNamed("testEvent", handleWithBlock: { (event) -> Void in
+            println(event.data)
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func send(){
+        self.api.triggerEvent("testEvent", onChannel: "50", data: ["foo": "bar"], socketID: nil)
     }
-
-
+    
 }
 
