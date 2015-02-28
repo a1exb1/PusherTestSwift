67446204deb04cc83a9f76bbf8af52854eb5ee7b
a1exb1 <alexbechmann@Alexs-MacBook-Pro.local>
2015-02-28 16:48:47 +0000

    messaging :)

--- PusherTestSwift/ViewController.swift
+++ PusherTestSwift/ViewController.swift
@@ -14,24 +14,29 @@ class ViewController: UIViewController, PTPusherDelegate {
     var channel: PTPusherChannel = PTPusherChannel()
     var api: PTPusherAPI = PTPusherAPI()
     
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var input: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "send")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Compose, target: self, action: "send")
         
         self.client = PTPusher.pusherWithKey("3bed0224e216f7d3c3a4", delegate: self)
         self.client?.connect()
         self.api = PTPusherAPI(key: "3bed0224e216f7d3c3a4", appID: "109110", secretKey: "c1e850ac3781ed00bf63")
         
         self.channel = self.client!.subscribeToChannelNamed("50")
-        self.channel.bindToEventNamed("testEvent", handleWithBlock: { (event) -> Void in
-            println(event.data)
+        self.channel.bindToEventNamed("testEvent", handleWithBlock: { (response) -> Void in
+            println(response.data)
+            self.lbl.text = response.data["message"] as? String
         })
     }
 
-
     func send(){
-        self.api.triggerEvent("testEvent", onChannel: "50", data: ["foo": "bar"], socketID: nil)
+        self.api.triggerEvent("testEvent", onChannel: "50", data: ["message": self.input.text], socketID: nil)
+        self.input.text = ""
     }
     
 }
